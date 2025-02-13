dc818cbb1c4d5529ad9b6124fb48e627b453943d
Mrt1ns <72765821+Mrt1ns@users.noreply.github.com>
2025-02-12 21:46:07 -0300

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,17 +1,14 @@
 variable "cluster_name" {
   description = "Nome do cluster ECS"
   type        = string
-  default = "app-prod-v2"
 }
 
 variable "subnets_id" {
   description = "Subnets IDs"
   type        = list(string)
-  default = [ "subnet-04774a57d61b370b3","subnet-08f4cb9f89b5b8983","subnet-0f2c78096c7b50445" ]
 }
 
 variable "vpc_id" {
   description = "VPC ID"
   type        = string
-  default = "app-prod-v2"
 }
