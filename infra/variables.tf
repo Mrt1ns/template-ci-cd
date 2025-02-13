ec0af81cf40234bb95a6f73b54c5c1ef3a6795ad
Mrt1ns <72765821+Mrt1ns@users.noreply.github.com>
2025-02-12 21:54:37 -0300

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -12,14 +12,3 @@ variable "vpc_id" {
   description = "VPC ID"
   type        = string
 }
-
-# Adicionando os valores diretamente
-cluster_name = "app-prod-v2"
-
-vpc_id = "vpc-04b23a0d543054da1"
-
-subnets_id = [
-  "subnet-04774a57d61b370b3",
-  "subnet-08f4cb9f89b5b8983",
-  "subnet-0f2c78096c7b50445"
-]
